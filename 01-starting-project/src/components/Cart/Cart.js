5b81a51963c3a2ba470e5df8b2e44d2e93a655ed
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-17 22:50:10 +0530

    first commit

--- 01-starting-project/src/components/Cart/Cart.js
+++ 01-starting-project/src/components/Cart/Cart.js
@@ -1,11 +1,12 @@
-import { useContext } from 'react';
-
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
+import { useContext, useState } from "react";
+import Checkout from "./Checkout";
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const [isCheckdOut, setIsChgeckedout ]= useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -20,7 +21,7 @@ const Cart = (props) => {
   };
 
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
@@ -33,7 +34,17 @@ const Cart = (props) => {
       ))}
     </ul>
   );
+const orderHandler = ()=>{
+  setIsChgeckedout(true);
 
+}
+
+const modalAction = <div className={classes.actions}>
+<button className={classes["button--alt"]} onClick={props.onClose}>
+  Close
+</button>
+{hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+</div>
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -41,12 +52,10 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+     {isCheckdOut && <Checkout onCancel={props.onClose}/>}
+     {!isCheckdOut && modalAction
+       }
+     
     </Modal>
   );
 };
